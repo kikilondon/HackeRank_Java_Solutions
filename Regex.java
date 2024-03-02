39cc7c998be0843f99ead871abc6f9687bfb34fc
Chiara Pigoni <m.chiarapigoni@gmail.com>
2024-03-02 16:10:08 +0000

    regex modified

--- Regex.java
+++ Regex.java
@@ -13,18 +13,5 @@ class Solution{
     }
 }
 class MyRegex{
-    String pattern = "\\b(?:\\d{1,3}\\.){3}\\d{1,3}\\b";
-
-}
-
-//"\\b(?:\\d{1,3}\\.){3}\\d{1,3}\\b";let's break down this :
-
-//  \\b this at the end and beginning is a word bundary that check that the string is 
-//not part of a larger sequence of characters tha might contain other periods or digits 
-
-//'(?:\\d{1,3}\\.){3}': This is a non-capturing group that matches three occurrences of 1 to 3 digits followed by a period.
-// The ?: at the beginning of the group makes it a non-capturing group, meaning that it doesn't capture the matched text.
-// The {3} at the end of the group specifies that the group should be repeated exactly three times.
-//This represents the first three parts of the IP address.
-
-//\\d{1,3}: This matches the last part of the IP address, which can also be 1 to 3 digits.
+    String pattern = "\\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b";
+};
